b01d1507804ef9d9b8a796749e870816ba237885
Jojoe Ainoo <43615811+jojoe-ainoo@users.noreply.github.com>
2021-11-25 17:30:14 +0000

    Add files via upload

---
+++ src/main/java/com/easyout/easytrade/EasyTradeApplication.java
@@ -0,0 +1,13 @@
+package com.easyout.easytrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyTradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easyout/easytrade/EasyTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyout.easytrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
