e376d03621f62b2d27703c93f6877c85f71f744d
Jojoe Ainoo <jojoeainoo@Obsidians-MacBook-Pro.local>
2021-11-25 19:08:30 +0000

    hello trade

--- src/main/java/com/easyout/easytrade/EasyTradeApplication.java
+++ src/main/java/com/easyout/easytrade/EasyTradeApplication.java
@@ -2,12 +2,19 @@ package com.easyout.easytrade;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class EasyTradeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EasyTradeApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+		public String hello() {
+		return "Hello Trade";
+	}
 }
