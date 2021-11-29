773bc496d4edfa2a5875f959a1bdc6df390676f8
Jojoe Ainoo <jojoeainoo@Obsidians-MBP.mshome.net>
2021-11-29 12:27:39 +0000

    test connections

---
+++ src/main/java/com/easyout/easytrade/models/Stock.java
@@ -0,0 +1,9 @@
+//package com.easyout.easytrade.models;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.Table;
+//
+//@Table(name = "stock")
+//@Entity
+//public class Stock {
+//}
